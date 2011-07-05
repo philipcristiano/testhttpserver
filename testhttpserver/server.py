3728eb4a944022892c799f25b97df29bcdc7cdd4
Philip Cristiano <git@philipcristiano.com>
2011-07-05 17:41:25 -0400

    Store request headers and content for any method.

--- testhttpserver/server.py
+++ testhttpserver/server.py
@@ -1,6 +1,7 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
 
+
 class Server(object):
 
     def __init__(self, port, response_status=200, response_content=''):
@@ -8,20 +9,22 @@ class Server(object):
 
         class Handler(BaseHTTPRequestHandler):
 
-            def do_POST(handler):
-                self.post_headers = handler.headers
-                length = int(handler.headers['Content-Length'])
-                self.post_data = handler.rfile.read(length)
-
-                handler.respond()
-
-            def do_GET(handler):
-                handler.respond()
-
             def respond(handler):
                 handler.send_response(response_status)
                 handler.end_headers()
                 handler.rfile.write(response_content)
+
+            def request_handler(handler):
+                self.request_headers = handler.headers
+                length = int(handler.headers.get('Content-Length', 0))
+                self.request_content = handler.rfile.read(length)
+
+                handler.respond()
+
+            def __getattr__(self, name):
+                if not name.startswith('do_'):
+                    raise AttributeError
+                return self.request_handler
 
         self.server = HTTPServer(('localhost', port), Handler)
         self.server.timeout = 5
--- tests/test_server.py
+++ tests/test_server.py
@@ -14,6 +14,8 @@ class BaseHTTPServerTest(object):
     def teardown_class(cls):
         cls.server.join()
 
+    def should_response_with_status_code_200(self):
+        assert self.response.status_code == 200
 
 class WhenGettingData(BaseHTTPServerTest):
 
@@ -35,7 +37,10 @@ class WhenPostingData(BaseHTTPServerTest):
     @classmethod
     def setup_class(cls, port=8020):
         BaseHTTPServerTest.setup_class(port)
+        cls.response = requests.post('http://localhost:8020', data='POST DATA')
 
-    def should_save_post_data(self):
-        requests.post('http://localhost:8020', data='POST DATA')
-        assert self.server.post_data == 'POST DATA'
+    def should_save_post_content(self):
+        assert self.server.request_content == 'POST DATA'
+
+    def should_save_headers(self):
+        print self.server.request_headers
